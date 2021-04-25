e05ea1bef10c39c3f152190caaa8fd7f48f26643
Gonzalo Martinez <gmartinez312@gmail.com>
2021-04-24 22:14:03 -0300

    costoya demo

---
+++ costoya/precache-manifest.4f6e4b7371901eb975d15200b6a5e9b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77dca8f13d5eff7ee66c27ac636e9e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c53b1d6cf6cf15805ec",
+    "url": "/static/css/main.2462711d.chunk.css"
+  },
+  {
+    "revision": "662e154f0d3f7ece5271",
+    "url": "/static/js/2.d5b90c2c.chunk.js"
+  },
+  {
+    "revision": "3c53b1d6cf6cf15805ec",
+    "url": "/static/js/main.e8f799c3.chunk.js"
+  },
+  {
+    "revision": "2836a8ebe84afd07513c",
+    "url": "/static/js/runtime-main.e9bf4e41.js"
+  }
+]);
