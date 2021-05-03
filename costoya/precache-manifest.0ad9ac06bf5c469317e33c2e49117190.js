3e73102e0ed5c7d2e17e642aa6e55c3433f5a916
Gonzalo Martinez <gmartinez312@gmail.com>
2021-05-03 17:54:28 -0300

    fix pantallas grandes

--- costoya/precache-manifest.0ad9ac06bf5c469317e33c2e49117190.js
+++ costoya/precache-manifest.0ad9ac06bf5c469317e33c2e49117190.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c6d27fa190d7f6ee421ab59be4e8f244",
+    "revision": "57fb5b3d76ccd834c7647dc3361ac02b",
     "url": "/index.html"
   },
   {
-    "revision": "959c6cda140414743ee0",
-    "url": "/static/css/main.7b773f23.chunk.css"
+    "revision": "89938afbc694b3a3e71c",
+    "url": "/static/css/main.6cd7188d.chunk.css"
   },
   {
     "revision": "daf90365b8dd399f3fdb",
     "url": "/static/js/2.4c49c499.chunk.js"
   },
   {
-    "revision": "959c6cda140414743ee0",
-    "url": "/static/js/main.602c7632.chunk.js"
+    "revision": "89938afbc694b3a3e71c",
+    "url": "/static/js/main.18d71e43.chunk.js"
   },
   {
     "revision": "5da5c12902685697d4fa",
--- costoya/service-worker.js
+++ costoya/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4345128b2c82da2fe9434a2c4292e13d.js"
+  "/precache-manifest.0ad9ac06bf5c469317e33c2e49117190.js"
 );
 
 self.addEventListener('message', (event) => {
